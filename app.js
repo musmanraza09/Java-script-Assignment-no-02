c577e063418e96f3c4780682afd230dd14650958
musmanraza09 <musmanraza09@gmail.com>
2025-07-19 21:28:43 +0500

    Java Script Assigment No 02

---
+++ app.js
@@ -0,0 +1,161 @@
+//Assigment no 02
+//console.log("Javascript is Connected!");
+
+//Task No 01
+//var city_name = prompt ("Enter Your City Name");
+
+//if(city_name === "Karachi"){
+//    console.log("Welcome to the City Of Lights!");
+    
+//}
+
+//Task No 02
+// var gender = prompt ("Please Enter Your Gender");
+// if(gender === "Male"){
+//     console.log("Good Morning Sir!");
+    
+// }
+// else if(gender === "Female"){
+//     console.log("Good Morning Ma'am");
+// }
+// else{
+//     console.log("Please Enter Correct Gender")
+// }
+
+//Task No 03
+// var signal_color = prompt ("Enter traffic signal Color!");
+// if(signal_color === "Red"){
+//     console.log("Must Stop");
+    
+// } 
+// else if(signal_color === "Yellow"){
+//     console.log("Ready to Move");
+// }
+// else if (signal_color === "Green"){
+//     console.log("Move Now");
+// }
+// else {
+//     console.log("Please Enter correct signal color");
+// }
+
+//Task No 04
+
+// var remaining_fuel = prompt ("Enetr remaining Fuel in the car");
+// if(remaining_fuel < 0.25){
+//     console.log("Please Refill the Fuel in Your Car");
+    
+// }
+
+//Task NO 05
+
+//for A
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+//For B
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+//For c
+
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+//For D
+
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+//For E
+
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+//For F
+
+// if("car" < "cat"){
+//alert("car is smaller than cat");
+//}
+
+//Task No 05
+
+//  var subject_1 = +prompt("Enter marks of Subject 01.");
+//  var subject_2 = +prompt("Enter marks of Subject 02.");
+//  var subject_3 = +prompt("Enter marks of Subject 03.");
+//  var totalObtainedMarks = subject_1 + subject_2 + subject_3;
+//  var totalMarks = 300;
+//  var percentage = totalObtainedMarks / totalMarks * 100;
+
+//  document.write("<h1>Marks Sheet</h1>")
+
+//  if(percentage >= 80 && percentage <= 100){
+//      document.write("Total Marks : " + totalMarks + "<br>" + "Marks Obtained : " + totalObtainedMarks + "<br>" + "Percentage : " + percentage + "%" + "<br>" + "Grade : A-One <br> Remarks : Excellent" )
+//  }else if(percentage >= 70 && percentage <= 79){
+//      document.write("Total Marks : " + totalMarks + "<br>" + "Marks Obtained : " + totalObtainedMarks + "<br>" + "Percentage : " + percentage + "%" + "<br>" + "Grade : A <br> Remarks : Good" )
+//  }else if(percentage >= 60 && percentage <= 69){
+//      document.write("Total Marks : " + totalMarks + "<br>" + "Marks Obtained : " + totalObtainedMarks + "<br>" + "Percentage : " + percentage + "%" + "<br>" + "Grade : B <br> Remarks : You Need to Improve" )
+//  }else if(percentage < 60){
+//      document.write("Total Marks : " + totalMarks + "<br>" + "Marks Obtained : " + totalObtainedMarks + "<br>" + "Percentage : " + percentage + "%" + "<br>" + "Grade : F <br> Remarks : Sorry" )
+//  }else{
+//      console.log("Enter correct marks Or enter numbers in Numeric form");
+//  }
+
+//Task No 06
+
+// var secretNum = 3;
+// var guess = +prompt("Enter the Number.");
+// if(secretNum === guess){
+//     console.log("“Bingo! Correct answer”.");
+// }else if(secretNum + 1 === guess ){
+//     console.log("Close enough to the correct answer.")
+// }
+
+//Task No 07
+
+// var secretNum = 3;
+// var guess = +prompt("Enter the Number.");
+// if(secretNum === guess){
+//     console.log("“Bingo! Correct answer”.");
+// }else if(secretNum + 1 === guess ){
+//     console.log("Close enough to the correct answer.")
+// }
+
+//Task No 08
+
+ var number = +prompt("Enter Number.");
+ if(number % 3 === 0){
+     console.log("The number " + number + " is divisible by 3.");
+ }
+
+ //Task No 09 
+ 
+ 
+
+
+
+
+
